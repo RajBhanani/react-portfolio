78105fd4fffc87ebe2cb2314563ef8ce9e49345e
Raj Bhanani <rjbhanani@gmail.com>
2023-04-09 19:24:11 +0530

    Almost complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Navbar.css";
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 function Navbar() {
+  const location = useLocation();
+  const [{ navBackgroundColor, hoverStyle }, setNavStyle] = useState("");
+  const [isHover, setIsHover] = useState({
+    home: false,
+    about: false,
+    experience: false,
+    projects: false,
+    contact: false,
+  });
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setNavStyle({
+        navBackgroundColor: "#201F41",
+        hoverStyle: { backgroundColor: "#2a294f", transition: "ease-out 0.2s" },
+      });
+    }
+    if (location.pathname === "/about") {
+      setNavStyle({
+        navBackgroundColor: "#422D53",
+        hoverStyle: { backgroundColor: "#4c375d", transition: "ease-out 0.2s" },
+      });
+    }
+    if (location.pathname === "/experience") {
+      setNavStyle({
+        navBackgroundColor: "#912E54",
+        hoverStyle: { backgroundColor: "#a03b62", transition: "ease-out 0.2s" },
+      });
+    }
+    if (location.pathname === "/projects") {
+      setNavStyle({
+        navBackgroundColor: "#D1788F",
+        hoverStyle: { backgroundColor: "#cd6b85", transition: "ease-out 0.2s" },
+      });
+    }
+    if (location.pathname === "/contact") {
+      setNavStyle({
+        navBackgroundColor: "#DD7C86",
+        hoverStyle: { backgroundColor: "#da6c77", transition: "ease-out 0.2s" },
+      });
+    }
+  }, [location]);
+
   return (
-    <header>
+    <header style={{ backgroundColor: navBackgroundColor }}>
       <div className="logo">
         <img
           src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.HRCx3YTKjKJQ7BWXHod5uwHaFj%26pid%3DApi&f=1&ipt=dbecb0da5e271067f5b670373c8a8320e9dc9d0b3be8de5683b431fbabc961df&ipo=images"
@@ -14,11 +57,61 @@ function Navbar() {
       </div>
       {/* <div className="nav"> */}
       <div className="items">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/experience">Experience</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/contact">Contact</Link>
+        <NavLink
+          className="item"
+          onMouseEnter={(prev) => {
+            setIsHover({ ...prev, home: true });
+          }}
+          onMouseLeave={(prev) => setIsHover({ ...prev, home: false })}
+          style={isHover.home ? hoverStyle : {}}
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className="item"
+          onMouseEnter={(prev) => {
+            setIsHover({ ...prev, about: true });
+          }}
+          onMouseLeave={(prev) => setIsHover({ ...prev, about: false })}
+          style={isHover.about ? hoverStyle : {}}
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink
+          className="item"
+          onMouseEnter={(prev) => {
+            setIsHover({ ...prev, experience: true });
+          }}
+          onMouseLeave={(prev) => setIsHover({ ...prev, experience: false })}
+          style={isHover.experience ? hoverStyle : {}}
+          to="/experience"
+        >
+          Experience
+        </NavLink>
+        <NavLink
+          className="item"
+          onMouseEnter={(prev) => {
+            setIsHover({ ...prev, projects: true });
+          }}
+          onMouseLeave={(prev) => setIsHover({ ...prev, projects: false })}
+          style={isHover.projects ? hoverStyle : {}}
+          to="/projects"
+        >
+          Projects
+        </NavLink>
+        <NavLink
+          className="item"
+          onMouseEnter={(prev) => {
+            setIsHover({ ...prev, contact: true });
+          }}
+          onMouseLeave={(prev) => setIsHover({ ...prev, contact: false })}
+          style={isHover.contact ? hoverStyle : {}}
+          to="/contact"
+        >
+          Contact
+        </NavLink>
       </div>
       {/* </div> */}
     </header>
